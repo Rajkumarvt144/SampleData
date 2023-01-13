49e70447b4140921d04d088d385f61a63213f0c5
Rajkumarvt144 <rajkumarvt144@gmail.com>
2023-01-13 11:36:34 +0530

    new project added

--- src/com/test/Sample.java
+++ src/com/test/Sample.java
@@ -1,6 +1,15 @@
 package com.test;
 
 public class Sample {
+	public static void main(String[] args) {
+		System.out.println(1);
+		System.out.println(2);
+		System.out.println(3);
+		System.out.println(4);
+		System.out.println(5);
+		System.out.println(6);
+		System.out.println(7);
 		
-	
+	}
+
 }
